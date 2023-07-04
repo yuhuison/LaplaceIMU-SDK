0918f69a4435b7317b96ca4686c3448e805e98e4
yuhuison <yuhuison@live.com>
2023-07-04 17:51:27 +0800

    add comment

--- main.py
+++ main.py
@@ -1,71 +1,69 @@
-import dataclasses
-import struct
-import threading
-from math import isnan
-import time
-import socket
-import torch
-
+import dataclasses  # 导入dataclasses模块，用于创建不可变的数据类
+import struct  # 导入struct模块，用于处理二进制数据和C结构体
+import threading  # 导入threading模块，用于实现多线程编程
+from math import isnan  # 从math模块导入isnan函数，用于检查是否为NaN值
+import time  # 导入time模块，用于处理时间相关操作
+import socket  # 导入socket模块，用于网络通信
+import torch  # 导入torch模块，用于深度学习相关操作
 
 @dataclasses.dataclass
 class ImuData:
-    accX: float = 0.0
-    accY: float = 0.0
-    accZ: float = 0.0
-    i: float = 0.0
-    j: float = 0.0
-    k: float = 0.0
-    real: float = 1
+    accX: float = 0.0  # 加速度X分量，默认为0.0
+    accY: float = 0.0  # 加速度Y分量，默认为0.0
+    accZ: float = 0.0  # 加速度Z分量，默认为0.0
+    i: float = 0.0  # i分量，默认为0.0
+    j: float = 0.0  # j分量，默认为0.0
+    k: float = 0.0  # k分量，默认为0.0
+    real: float = 1  # 实数部分，默认为1
 
-
-class LaprasImu:
-    data: ImuData = ImuData(0, 0, 0, 0, 0, 0)
-    last_data_time:int = 0
-    ggid = 0
-    fps = 0
+class LaplaceImu:
+    data: ImuData = ImuData(0, 0, 0, 0, 0, 0)  # 创建ImuData对象data，并初始化各属性为0
+    last_data_time: int = 0  # 上一次数据接收时间，默认为0
+    ggid = 0  # 设备ID，默认为0
+    fps = 0  # 帧率，默认为0
 
     def __init__(self, imuid):
-        self.ggid = imuid
-
+        self.ggid = imuid  # 初始化ggid为传入的imuid
 
 class IMUSet:
-    imus: dict = dict()
-    living:bool = True
+    imus: dict = dict()  # 创建一个字典用于存储Imu对象，键为ggid，值为LaplaceImu对象
+    living: bool = True  # 表示IMUSet对象是否处于运行状态，默认为True
+
     def __init__(self):
-        self.t = threading.Thread(target=self.running)
+        self.t = threading.Thread(target=self.running)  # 创建一个线程对象，目标函数为running
 
     def start(self):
-        self.t.start()
+        self.t.start()  # 启动线程
 
     def stop(self):
-        self.living = False
+        self.living = False  # 停止IMUSet对象的运行
 
     def running(self):
         def mills():
-            return int(round(time.time() * 1000))
+            return int(round(time.time() * 1000))  # 获取当前时间的毫秒表示
 
-        HOST = '0.0.0.0'
-        PORT = 42605
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((HOST, PORT))
-        tt = mills()
-        while self.living:
-            data, addr = sock.recvfrom(1024)
-            msg = struct.unpack("<ifffffffL", data)
-            ggid, i, j, k, real, accX, accY, accZ, mill_time = msg
+        HOST = '0.0.0.0'  # 主机IP地址
+        PORT = 42605  # 端口号
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 创建UDP套接字
+        sock.bind((HOST, PORT))  # 绑定IP地址和端口号
+        tt = mills()  # 获取当前时间的毫秒表示
+        while self.living:  # 当IMUSet对象处于运行状态时
+            data, addr = sock.recvfrom(1024)  # 接收数据和发送方的地址
+            msg = struct.unpack("<ifffffffL", data)  # 解析接收到的二进制数据
+            ggid, i, j, k, real, accX, accY, accZ, mill_time = msg  # 将解析后的数据赋值给变量
             if real == 0:
-                real = 1
-            if not (isnan(ggid) or isnan(accX) or isnan(accY) or isnan(
-                    accZ)):
+                real = 1  # 如果real为0，则将其设置为1
+            if not (isnan(ggid) or isnan(accX) or isnan(accY) or isnan(accZ)):
+                # 如果ggid、accX、accY、accZ中有任何一个值为NaN，则不执行以下代码
                 if ggid not in self.imus.keys():
-                    self.imus[ggid] = LaprasImu(ggid)
-                imu = self.imus[ggid]
+                    self.imus[ggid] = LaplaceImu(ggid)  # 如果ggid不在imus的键中，则创建一个LaplaceImu对象
+                imu = self.imus[ggid]  # 获取对应ggid的LaplaceImu对象
                 if mill_time > imu.last_data_time:
-                    imu.last_data_time = mill_time
-                    imu.fps = imu.fps + 1
-                    imu.data = ImuData(accX, accY, accZ, i, j, k, real)
-                    if mills() - tt > 1000:
+                    imu.last_data_time = mill_time  # 更新最后接收数据的时间
+                    imu.fps = imu.fps + 1  # 帧率加1
+                    imu.data = ImuData(accX, accY, accZ, i, j, k, real)  # 更新imu的数据
+                    if mills() - tt > 1000:  # 如果当前时间与tt的差值大于1秒
                         for imu in self.imus.values():
                             if imu.fps != 0:
-                                imu.fps = 0
-                        tt = mills()
+                                imu.fps = 0  # 将所有imu的帧率重置为0
+                        tt = mills()  # 更新tt为当前时间的毫秒表示
--- setting.py
+++ setting.py
@@ -1,30 +1,31 @@
-import serial
-import time
-import serial.tools.list_ports
-
+import serial  # 导入serial模块，用于串口通信
+import time  # 导入time模块，用于处理时间相关操作
+import serial.tools.list_ports  # 导入serial.tools.list_ports模块，用于获取可用串口列表
 
 def start_assign_wifi(ssid, password):
-    ret = []
-    searcher = serial.tools.list_ports.comports()
-    for port in searcher:
+    ret = []  # 创建一个空列表，用于存储配网成功的传感器串口号
+    searcher = serial.tools.list_ports.comports()  # 获取可用串口列表
+    for port in searcher:  # 遍历每个可用串口
         port_name = port.device  # 获取串口号
         guid = port.hwid  # 获取总线类型GUID
-        for dev_id in ["VID_303A&PID_1001"]:
-            if dev_id in guid:
+        for dev_id in ["VID_303A&PID_1001"]:  # 遍历每个设备ID
+            if dev_id in guid:  # 如果设备ID在总线类型GUID中
                 ser = serial.Serial(port_name, 9600, parity=serial.PARITY_NONE, bytesize=8,
-                                    stopbits=serial.STOPBITS_ONE)
-                ser.open()
-                time.sleep(0.4)
-                buffer = ser.read_all().decode()
+                                    stopbits=serial.STOPBITS_ONE)  # 打开串口连接
+                ser.open()  # 打开串口连接
+                time.sleep(0.4)  # 等待0.4秒，确保串口连接稳定
+                buffer = ser.read_all().decode()  # 读取串口接收缓冲区的数据并解码为字符串
                 if "ESP-ROM:esp32c3" in buffer or "[LaplaceMoCap]" in buffer:
+                    # 如果缓冲区中包含"ESP-ROM:esp32c3"或"[LaplaceMoCap]"
                     ser.write(("#=#20001#=#" + ssid + "#=#" + password + "#=#" + "\n").encode())
-                    time.sleep(1)
-                    buffer = ser.read_all().decode()
+                    # 向串口发送配网指令，格式为"#=#20001#=#ssid#=#password#=#"
+                    time.sleep(1)  # 等待1秒，确保配网指令发送完成并接收到响应
+                    buffer = ser.read_all().decode()  # 读取串口接收缓冲区的数据并解码为字符串
                     if "[CODE 20001 SET WIFI 200]" in buffer:
-                        ret.append(port_name)
-                ser.close()
+                        # 如果缓冲区中包含"[CODE 20001 SET WIFI 200]"
+                        ret.append(port_name)  # 将串口号添加到配网成功的列表中
+                ser.close()  # 关闭串口连接
 
-    return ret
+    return ret  # 返回配网成功的传感器串口号列表
 
-
-print(start_assign_wifi("123", "456"))
+print(start_assign_wifi("123", "456"))  # 执行配网操作并打印配网成功的传感器串口号列表
